4d505ca23b1498bfa5ec0f943d73e3b9dd647854
Lord-Leo <130584018+Lord-Leo@users.noreply.github.com>
2023-11-17 22:05:18 +0600

    Add files via upload

---
+++ Game with integers.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N,a;
+    cin >>N;
+
+    for(int i=0; i<N; i++)
+    {
+        cin >>a;
+        if(a%3==0)
+        {
+            cout << "Second" << endl;
+        }
+        else
+        {
+            cout << "First" << endl;
+        }
+    }
+    return 0;
+}
